1c1bb73284d83b629526e6161a3227ee3f3b1b56
Naomi Katz <naykatz@gmail.com>
2019-02-17 19:13:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def dictionary
+dictionary = {
+    "hello" => "hi",
+    "to" => 2,
+    "two" => 2,
+    "too" => 2,
+    "for" => 4,
+    "four" => 4,
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  new_tweet = []
+  new_tweet << tweet.split.collect do |word|
+    if dictionary.keys.include? word.downcase
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  new_tweet.join(" ")
+end
+
+ 
+
